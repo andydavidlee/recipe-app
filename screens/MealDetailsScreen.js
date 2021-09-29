89a0441103ec27def64a57e7ccd5d14b6dc92f7f
Andrew Lee <andrew.lee@rolleaseacmeda.com>
2021-09-29 12:58:25 +1000

    Starting to add Redux

--- screens/MealDetailsScreen.js
+++ screens/MealDetailsScreen.js
@@ -1,8 +1,8 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
-import { MEALS } from '../data/dummy-data'
+import { useSelector } from 'react-redux'
 import HeaderButton from '../components/HeaderButton'
 import DefaultText from '../components/DefaultText'
 
@@ -15,9 +15,14 @@ const ListItem = (props) => {
 }
 
 const MealDetailsScreen = (props) => {
+	const availableMeals = useSelector((state) => state.meals.meals)
 	const mealId = props.navigation.getParam('mealId')
 
-	const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+	const selectedMeal = availableMeals.find((meal) => meal.id === mealId)
+
+	useEffect(() => {
+		props.navigation.setParams({ mealTitle: selectedMeal.title })
+	}, [selectedMeal])
 
 	return (
 		<ScrollView>
@@ -40,10 +45,11 @@ const MealDetailsScreen = (props) => {
 }
 
 MealDetailsScreen.navigationOptions = (navigationData) => {
-	const mealId = navigationData.navigation.getParam('mealId')
-	const selectedMeal = MEALS.find((meal) => meal.id === mealId)
+	// const mealId = navigationData.navigation.getParam('mealId')
+	const mealTitle = navigationData.navigation.getParam('mealTitle')
+	// const selectedMeal = MEALS.find((meal) => meal.id === mealId)
 	return {
-		headerTitle: selectedMeal.title,
+		headerTitle: mealTitle,
 		headerRight: (
 			<HeaderButtons HeaderButtonComponent={HeaderButton}>
 				<Item title='favourite' iconName='ios-star' onPress={() => {}} />
